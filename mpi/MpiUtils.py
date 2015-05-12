9fdbfc9e4d22e740433f54401e2b702a2b37cdbd
Henry Milner <henry@conviva.com>
2015-05-11 17:29:26 -0700

    Fix random seed generation for machines with 32-bit numpy.

--- mpi/MpiUtils.py
+++ mpi/MpiUtils.py
@@ -1,12 +1,16 @@
 import numpy as np
 import sys
 
+# Some versions of numpy only support seeds up to this.  This is unfortunate,
+# since collisions can make our algorithm fail.
+MAXIMUM_RANDOM_SEED = 2**32-1
+
 # Return a new RandomState whose seed is a deterministic function of the
 # seed in @rand and the machine ID, and which is different for different
 # machines.  Useful if we want to run a deterministic distributed job without
 # actually using the same seed on every machine.
 def reseed(comm, rand):
-  return np.random.RandomState(int((rand.randint(0, sys.maxint) + 53*comm.rank) % sys.maxint))
+  return np.random.RandomState(int((rand.randint(0, sys.maxint) + 53*comm.rank) % MAXIMUM_RANDOM_SEED))
 
 # Return a new RandomState whose seed is a deterministic function of @seed
 # and of @idx.  Useful for generating a range of random values that does not
@@ -14,7 +18,7 @@ def reseed(comm, rand):
 #   seed = rand.randint()
 #   makeRange(10).map(lambda item: makeRandForItem(seed, item))
 def makeRandForItem(seed, idx):
-  return np.random.RandomState(int((seed + 7*idx) % sys.maxint))
+  return np.random.RandomState(int((seed + 7*idx) % MAXIMUM_RANDOM_SEED))
 
 # Execute some computation embodied in @thunk on the master, or return None
 # on slaves.  This is just a convenience method for this common pattern in MPI
